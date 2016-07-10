f1dffe5b6547bbc2a91755c3d3818cf22d463129
Weerasak Chongnguluam <singpor@gmail.com>
2016-07-10 16:54:44 +0700

    Rename CopyPipeline to MessageHandler

--- matchingserver/main.go
+++ matchingserver/main.go
@@ -16,19 +16,19 @@ func main() {
 	}
 	defer l.Close()
 
-	waitingChan := make(chan *CopyPipeline, 1)
+	waitingChan := make(chan *MessageHandler, 1)
 	for {
 		conn, err := l.Accept()
 		if err != nil {
 			log.Fatal(err)
 		}
-		cp := &CopyPipeline{}
+		mh := &MessageHandler{}
 		select {
-		case waitingChan <- cp:
+		case waitingChan <- mh:
 			log.Println("New Waiting")
-			go func(conn net.Conn, cp *CopyPipeline, waitingChan chan *CopyPipeline) {
+			go func(conn net.Conn, mh *MessageHandler, waitingChan chan *MessageHandler) {
 				defer func() {
-					if w := cp.Writer(); w != nil {
+					if w := mh.Writer(); w != nil {
 						w.Close()
 					} else {
 						if len(waitingChan) > 0 {
@@ -37,63 +37,63 @@ func main() {
 					}
 					conn.Close()
 				}()
-				cp.SetReader(conn)
-				cp.StartCopy()
-			}(conn, cp, waitingChan)
-		case cp := <-waitingChan:
-			go func(conn net.Conn, cp *CopyPipeline) {
+				mh.SetReader(conn)
+				mh.StartCopy()
+			}(conn, mh, waitingChan)
+		case mh := <-waitingChan:
+			go func(conn net.Conn, mh *MessageHandler) {
 				defer func() {
-					if w := cp.Writer(); w != nil {
+					if w := mh.Writer(); w != nil {
 						w.Close()
 					}
 					conn.Close()
 				}()
 				log.Println("Match")
-				cp.SetWriter(conn)
-				w := cp.Reader()
-				cp = &CopyPipeline{}
-				cp.SetReader(conn)
-				cp.SetWriter(w)
-				cp.StartCopy()
-			}(conn, cp)
-			waitingChan = make(chan *CopyPipeline, 1)
+				mh.SetWriter(conn)
+				w := mh.Reader()
+				mh = &MessageHandler{}
+				mh.SetReader(conn)
+				mh.SetWriter(w)
+				mh.StartCopy()
+			}(conn, mh)
+			waitingChan = make(chan *MessageHandler, 1)
 		}
 	}
 }
 
-type CopyPipeline struct {
+type MessageHandler struct {
 	r io.ReadWriteCloser
 	w io.ReadWriteCloser
 
 	mu sync.RWMutex
 }
 
-func (cp *CopyPipeline) Reader() io.ReadWriteCloser {
-	cp.mu.RLock()
-	defer cp.mu.RUnlock()
-	return cp.r
+func (mh *MessageHandler) Reader() io.ReadWriteCloser {
+	mh.mu.RLock()
+	defer mh.mu.RUnlock()
+	return mh.r
 }
 
-func (cp *CopyPipeline) Writer() io.ReadWriteCloser {
-	cp.mu.RLock()
-	defer cp.mu.RUnlock()
-	return cp.w
+func (mh *MessageHandler) Writer() io.ReadWriteCloser {
+	mh.mu.RLock()
+	defer mh.mu.RUnlock()
+	return mh.w
 }
 
-func (cp *CopyPipeline) SetReader(r io.ReadWriteCloser) {
-	cp.mu.Lock()
-	defer cp.mu.Unlock()
-	cp.r = r
+func (mh *MessageHandler) SetReader(r io.ReadWriteCloser) {
+	mh.mu.Lock()
+	defer mh.mu.Unlock()
+	mh.r = r
 }
 
-func (cp *CopyPipeline) SetWriter(w io.ReadWriteCloser) {
-	cp.mu.Lock()
-	defer cp.mu.Unlock()
-	cp.w = w
+func (mh *MessageHandler) SetWriter(w io.ReadWriteCloser) {
+	mh.mu.Lock()
+	defer mh.mu.Unlock()
+	mh.w = w
 }
 
-func (cp *CopyPipeline) StartCopy() {
-	r := cp.Reader()
+func (mh *MessageHandler) StartCopy() {
+	r := mh.Reader()
 	if r == nil {
 		return
 	}
@@ -102,7 +102,7 @@ func (cp *CopyPipeline) StartCopy() {
 		text := scanner.Text()
 		log.Println(text)
 
-		w := cp.Writer()
+		w := mh.Writer()
 		if w != nil {
 			_, err := fmt.Fprintln(w, text)
 			if err != nil {
